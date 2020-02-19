0cd1ced0142e4654e3e78e46f3041d47e8ccfc55
trevel1959 <55749794+trevel1959@users.noreply.github.com>
2020-02-20 01:30:59 +0900

    파일을 통한 역목록 입력
    이제 노가다만 남았다

--- koreanRandomStation/main.cpp
+++ koreanRandomStation/main.cpp
@@ -3,6 +3,35 @@
 #include <stdlib.h>
 #include <map>
 #include <utility>
-
+#include <string>
+#include <fstream>
 using namespace std;
 
+int make_stationArray(map<int, pair<string, string>>& staArr);
+
+int main() {
+	srand(time(NULL));
+	map<int, pair<string, string>> staArr;
+	make_stationArray(staArr);
+
+	cout << staArr[100].second << endl;
+}
+
+int make_stationArray(map<int, pair<string, string>>& staArr) {
+	ifstream readFile;
+	readFile.open("stationList.txt");
+	string tmpNo, realNo, name;
+
+	if (readFile.is_open()) {
+		readFile >> tmpNo >> realNo>> name;
+		cout << tmpNo << realNo << name << endl;
+	}
+	else {
+		cout << "역 목록을 불러올 수 없습니다." << endl;
+		return -1;
+	}
+
+	//staArr.insert(make_pair(100, make_pair("100", "소요산")));
+
+	return 0;
+}
