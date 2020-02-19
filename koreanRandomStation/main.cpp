d2a57dc8f080608b3733dae2d0224773a9525fd1
trevel1959 <55749794+trevel1959@users.noreply.github.com>
2020-02-20 02:37:51 +0900

    기본 기능 완성
    엑셀 노가다 힘들었다

--- koreanRandomStation/main.cpp
+++ koreanRandomStation/main.cpp
@@ -7,31 +7,43 @@
 #include <fstream>
 using namespace std;
 
-int make_stationArray(map<int, pair<string, string>>& staArr);
+int make_stationArray(map<int, pair<string, string>>& staArr, int& startNum, int& endNum);
 
 int main() {
 	srand(time(NULL));
 	map<int, pair<string, string>> staArr;
-	make_stationArray(staArr);
+	int startNum, endNum, randNum;
+	make_stationArray(staArr, startNum, endNum);
 
-	cout << staArr[100].second << endl;
+	cout << "이번에 선택된 역은" << endl;
+	do {
+		randNum = rand() % (endNum - startNum + 1) + startNum;
+	} while (staArr[randNum].second == "");
+	cout << "<" <<  staArr[randNum].first << "> " << staArr[randNum].second << " 역 입니다." << endl;
+
 }
 
-int make_stationArray(map<int, pair<string, string>>& staArr) {
+int make_stationArray(map<int, pair<string, string>>& staArr, int& startNum, int& endNum) {
 	ifstream readFile;
 	readFile.open("stationList.txt");
 	string tmpNo, realNo, name;
 
 	if (readFile.is_open()) {
-		readFile >> tmpNo >> realNo>> name;
-		cout << tmpNo << realNo << name << endl;
+		readFile >> tmpNo >> realNo >> name;
+		staArr.insert(make_pair(stoi(tmpNo), make_pair(realNo, name)));
+		startNum = stoi(tmpNo);
+
+		while (readFile.eof() == false) {
+			readFile >> tmpNo >> realNo >> name;
+			staArr.insert(make_pair(stoi(tmpNo), make_pair(realNo, name)));
+			//cout << tmpNo << " " << staArr[stoi(tmpNo)].first << " " << staArr[stoi(tmpNo)].second << endl;
+		}
+
+		endNum = stoi(tmpNo);
+		return 0;
 	}
 	else {
 		cout << "역 목록을 불러올 수 없습니다." << endl;
 		return -1;
 	}
-
-	//staArr.insert(make_pair(100, make_pair("100", "소요산")));
-
-	return 0;
 }
